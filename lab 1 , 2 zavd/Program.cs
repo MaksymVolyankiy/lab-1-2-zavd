356c2d51fdaa5aa600f47f41db0a2205b0bdaf37
MV <MV@MACBEK>
2024-03-06 10:27:05 +0200

    Добавьте файлы проекта.

---
+++ lab 1 , 2 zavd/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+var dict = new Dictionary<string, List<int>>
+{
+    { "key1", new List<int> { 12, 27, 45 } },
+    { "key2", new List<int> { 16, 5 } },
+    { "key3", new List<int> { 66, 77, 88, 99 } }
+};
+var sums = new Dictionary<string, int>();
+
+foreach (var kvp in dict)
+{
+    sums[kvp.Key] = SumList(kvp.Value);
+}
+string jsonResult = JsonConvert.SerializeObject(sums, Formatting.Indented);
+Console.WriteLine(jsonResult);
+static int SumList(List<int> list)
+{
+int sum = 0;
+foreach (int num in list)
+{
+    sum += num;
+}
+return sum;
+}
